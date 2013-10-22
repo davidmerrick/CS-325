b2cea2a9ec015d0538d233ea52921f0d19f87bff
David Merrick <david@david-merrick.com>
2013-10-21 21:59:45 -0700

    Added inversion variables

--- Assn 2/algorithm2.c
+++ Assn 2/algorithm2.c
@@ -9,7 +9,6 @@
 typedef struct arraymeta{
 	int value;
 	int parent_array;
-	int position; //Position in parent array
 } arraymeta;
 
 typedef struct arraydiff{
@@ -28,8 +27,8 @@ int array1_size = 4;
 int array2_size = 4;
 
 int main(){	
-	int array1[] = {1,-2,-4,3};
-	int array2[] = {6,-5,1,4};
+	int array1[] = {1,-3,5,-7};
+	int array2[] = {-2,4,-6,8};
 
 	int totalsize = array1_size + array2_size;
 
@@ -42,7 +41,6 @@ int main(){
 		tempsum += array1[i];
 		sumarray[j].value = tempsum;
 		sumarray[j].parent_array = 1;
-		sumarray[j].position = i;
 		j++;
 	}
 	tempsum = 0;
@@ -51,7 +49,6 @@ int main(){
 		tempsum += array2[i];
 		sumarray[j].value = -tempsum;
 		sumarray[j].parent_array = 2;
-		sumarray[j].position = i;
 		j++;
 	}
 	
@@ -61,38 +58,38 @@ int main(){
 	
 	arraydiff smallestdiff, tempdiff;
 	
+	//These will invert the number if it's in the negative array
+	int inversion1 = 1;
+	int inversion2 = 1;
+	
 	//Initial case
 	int i = 0;
 	while(sumarray[i].parent_array == sumarray[i+1].parent_array){
 		i++;
 	}
 	if(sumarray[i].parent_array != sumarray[i+1].parent_array){
-		//Correct the value
 		if(sumarray[i].parent_array == 2){
-			sumarray[i].value = -sumarray[i].value;
+			inversion1 = -1;
+		} else if(sumarray[i+1].parent_array == 2){
+			inversion2 = -1;
 		}
-		if(sumarray[i+1].parent_array == 2){
-			sumarray[i+1].value = -sumarray[i+1].value;
-		}
-		smallestdiff.value = sumarray[i].value + sumarray[i+1].value;
-		smallestdiff.lower_bound = sumarray[i].value;
-		smallestdiff.upper_bound = sumarray[i+1].value;
+		smallestdiff.value = inversion1 * sumarray[i].value + inversion2 * sumarray[i+1].value;
+		smallestdiff.lower_bound = inversion1 * sumarray[i].value;
+		smallestdiff.upper_bound = inversion2 * sumarray[i+1].value;
 	}
 	for(i; i < totalsize; i++){
 		if(sumarray[i].parent_array != sumarray[i+1].parent_array){
-			//Correct the value
+			inversion1 = inversion2 = 1; //Reset
 			if(sumarray[i].parent_array == 2){
-				sumarray[i].value = -sumarray[i].value;
+				inversion1 = -1;
+			} else if(sumarray[i+1].parent_array == 2){
+				inversion2 = -1;
 			}
-			if(sumarray[i+1].parent_array == 2){
-				sumarray[i+1].value = -sumarray[i+1].value;
-			}
-			//Add and compare
-			tempdiff.value = sumarray[i].value + sumarray[i+1].value;
+			tempdiff.value = inversion1 * sumarray[i].value + inversion2 * sumarray[i+1].value;
 			if(abs(tempdiff.value) < abs(smallestdiff.value)){
 				smallestdiff.value = tempdiff.value;
-				smallestdiff.lower_bound = sumarray[i].value;
-				smallestdiff.upper_bound = sumarray[i+1].value;
+				smallestdiff.lower_bound = inversion1 * sumarray[i].value;
+				smallestdiff.upper_bound = inversion2 * sumarray[i+1].value;
 			}
 		}
 	}
@@ -153,7 +150,6 @@ void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right){
 	      temp[tmp_pos].value = numbers[left].value;
 		  //Todo: is this necessary?
 		  temp[tmp_pos].parent_array = numbers[left].parent_array;
-		  temp[tmp_pos].position = numbers[left].position;
 	      tmp_pos = tmp_pos + 1;
 	      left = left +1;
 	    }
@@ -162,7 +158,6 @@ void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right){
 	      temp[tmp_pos].value = numbers[mid].value;
 	      //Todo: is this necessary?
 		  temp[tmp_pos].parent_array = numbers[mid].parent_array;
-		  temp[tmp_pos].position = numbers[mid].position;
 	      tmp_pos = tmp_pos + 1;
 	      mid = mid + 1;
 	    }
@@ -172,7 +167,6 @@ void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right){
 	    temp[tmp_pos].value = numbers[left].value;
 	    //Todo: is this necessary?
 		temp[tmp_pos].parent_array = numbers[left].parent_array;
-		temp[tmp_pos].position = numbers[left].position;
 	    left = left + 1;
 	    tmp_pos = tmp_pos + 1;
 	  }
@@ -181,7 +175,6 @@ void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right){
 	    temp[tmp_pos].value = numbers[mid].value;
 	    //Todo: is this necessary?
 		temp[tmp_pos].parent_array = numbers[mid].parent_array;
-		temp[tmp_pos].position = numbers[mid].position;
 	    mid = mid + 1;
 	    tmp_pos = tmp_pos + 1;
 	  }
@@ -189,7 +182,6 @@ void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right){
 	  for (i=0; i <= num_elements; i++){
 	  	numbers[right].value = temp[right].value;
 		numbers[right].parent_array = temp[right].parent_array;
-		numbers[right].position = temp[right].position;
 	    right = right - 1;
 	  }
 }
