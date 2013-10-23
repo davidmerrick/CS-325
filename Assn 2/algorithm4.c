3fa6de9b9838f31f22d1f2cd273204d23d0735de
David Merrick <david@david-merrick.com>
2013-10-23 13:19:06 -0700

    This needs to be debugged

--- Assn 2/algorithm4.c
+++ Assn 2/algorithm4.c
@@ -6,11 +6,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct arraymeta{
+	int value;
+	int parent_array;
+} arraymeta;
+
+typedef struct arraydiff{
+	int value;
+	int lower_bound;
+	int upper_bound;
+} arraydiff;
+
 int *compute_sum_array(int *array, int size);
+int compare_sort(int *array, int size);
 void print_array(int *array, int size);
-int enumerate(int *array, int size);
 int recurse(int *array, int size);
 int lesser_of(int a, int b);
+void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right);
+void m_sort(arraymeta numbers[], arraymeta temp[], int left, int right);
+void mergeSort(arraymeta numbers[], arraymeta temp[], int array_size);
 
 int main(){
 	int size = 8;
@@ -26,7 +40,7 @@ int recurse(int *array, int size){
 	} else if(size == 2){
 		return lesser_of(array[0], array[1]);
 	} else if(size == 3 || size == 4){
-		return enumerate(array, size);
+		return compare_sort(array, size);
 	} else {
 		//Split the array
 		int array1_size = size/2;
@@ -50,11 +64,11 @@ int lesser_of(int a, int b){
 		return a;
 	} else {
 		return b;
-	}
-	
+	}	
 }
 
-int enumerate(int *array, int size){
+int compare_sort(int *array, int size){
+	//Split and reverse the array
 	int array1_size = size/2;
 	int array1[array1_size];
 	for(int i = 0; i < size/2; i++){
@@ -64,7 +78,7 @@ int enumerate(int *array, int size){
 	int array2_size = size - size/2;
 	int array2[array2_size];
 	for(int i = size/2; i < size; i++){
-		array2[j] = array[i];
+		array2[j] = -array[i];
 		j++;
 	}
 	
@@ -73,23 +87,71 @@ int enumerate(int *array, int size){
 	for(int t=1; t<=array2_size; t++){
 	  array2rev[t-1] = array2[array2_size-t];
 	}
+	
+	int totalsize = array1_size + array2_size;
 
 	//Compute sum arrays
-	int *sumarray1 = compute_sum_array(array1, array1_size);
-	int *sumarray2 = compute_sum_array(array2rev, array2_size);
+	//Dump positive array 1 sums into first half of sumarray
+	arraymeta *sumarray = malloc(totalsize * sizeof(arraymeta));
+	j = 0; //index for sumarray
+	int tempsum = 0;
+	for (int i=array1_size-1; i >= 0; i--){
+		tempsum += array1[i];
+		sumarray[j].value = tempsum;
+		sumarray[j].parent_array = 1;
+		j++;
+	}
+	tempsum = 0;
+	//Dump negative array 2 sums into second half of sumarray
+	for (int i=array2_size-1; i >= 0; i--){
+		tempsum += array2[i];
+		sumarray[j].value = -tempsum;
+		sumarray[j].parent_array = 2;
+		j++;
+	}
 	
-	//Find the smallest sum
-	int smallestsum = sumarray1[0] + sumarray2[0];
-	int tempsum;
-	for(int i = 1; i < array1_size; i++){
-		for(int j = 0; j < array2_size; j++){
-			tempsum = sumarray1[i] + sumarray2[j];
-			if(abs(tempsum) < abs(smallestsum)){
-				smallestsum = tempsum;
+	arraymeta temp[totalsize];
+	
+	mergeSort(sumarray, temp, totalsize);
+	
+	arraydiff smallestdiff, tempdiff;
+	
+	//These will invert the number if it's in the negative array
+	int inversion1 = 1;
+	int inversion2 = 1;
+	
+	//Initial case
+	int i = 0;
+	while(sumarray[i].parent_array == sumarray[i+1].parent_array){
+		i++;
+	}
+	if(sumarray[i].parent_array != sumarray[i+1].parent_array){
+		if(sumarray[i].parent_array == 2){
+			inversion1 = -1;
+		} else if(sumarray[i+1].parent_array == 2){
+			inversion2 = -1;
+		}
+		smallestdiff.value = inversion1 * sumarray[i].value + inversion2 * sumarray[i+1].value;
+		smallestdiff.lower_bound = inversion1 * sumarray[i].value;
+		smallestdiff.upper_bound = inversion2 * sumarray[i+1].value;
+	}
+	for(i; i < totalsize; i++){
+		if(sumarray[i].parent_array != sumarray[i+1].parent_array){
+			inversion1 = inversion2 = 1; //Reset
+			if(sumarray[i].parent_array == 2){
+				inversion1 = -1;
+			} else if(sumarray[i+1].parent_array == 2){
+				inversion2 = -1;
+			}
+			tempdiff.value = inversion1 * sumarray[i].value + inversion2 * sumarray[i+1].value;
+			if(abs(tempdiff.value) < abs(smallestdiff.value)){
+				smallestdiff.value = tempdiff.value;
+				smallestdiff.lower_bound = inversion1 * sumarray[i].value;
+				smallestdiff.upper_bound = inversion2 * sumarray[i+1].value;
 			}
 		}
 	}
-	return smallestsum;
+	return smallestdiff.value;
 }
 
 int *compute_sum_array(int *array, int size){
@@ -108,4 +170,70 @@ void print_array(int *array, int size){
 	for(int i = 0; i < size; i++){
 		printf("%d\n", array[i]);
 	}
+}
+
+
+void mergeSort(arraymeta numbers[], arraymeta temp[], int array_size){
+	m_sort(numbers, temp, 0, array_size - 1);
+}
+
+void m_sort(arraymeta numbers[], arraymeta temp[], int left, int right){
+	  int mid;
+
+	  if (right > left){
+		mid = (right + left) / 2;
+	    m_sort(numbers, temp, left, mid);
+	    m_sort(numbers, temp, mid+1, right);
+	    merge(numbers, temp, left, mid+1, right);
+	  }
+}
+
+void merge(arraymeta numbers[], arraymeta temp[], int left, int mid, int right){
+	  int i, left_end, num_elements, tmp_pos;
+
+	  left_end = mid - 1;
+	  tmp_pos = left;
+	  num_elements = right - left + 1;
+
+	  while ((left <= left_end) && (mid <= right))
+	  {
+	    if (numbers[left].value <= numbers[mid].value)
+	    {
+	      temp[tmp_pos].value = numbers[left].value;
+		  //Todo: is this necessary?
+		  temp[tmp_pos].parent_array = numbers[left].parent_array;
+	      tmp_pos = tmp_pos + 1;
+	      left = left +1;
+	    }
+	    else
+	    {
+	      temp[tmp_pos].value = numbers[mid].value;
+	      //Todo: is this necessary?
+		  temp[tmp_pos].parent_array = numbers[mid].parent_array;
+	      tmp_pos = tmp_pos + 1;
+	      mid = mid + 1;
+	    }
+	  }
+
+	  while (left <= left_end){
+	    temp[tmp_pos].value = numbers[left].value;
+	    //Todo: is this necessary?
+		temp[tmp_pos].parent_array = numbers[left].parent_array;
+	    left = left + 1;
+	    tmp_pos = tmp_pos + 1;
+	  }
+	
+	  while (mid <= right){
+	    temp[tmp_pos].value = numbers[mid].value;
+	    //Todo: is this necessary?
+		temp[tmp_pos].parent_array = numbers[mid].parent_array;
+	    mid = mid + 1;
+	    tmp_pos = tmp_pos + 1;
+	  }
+
+	  for (i=0; i <= num_elements; i++){
+	  	numbers[right].value = temp[right].value;
+		numbers[right].parent_array = temp[right].parent_array;
+	    right = right - 1;
+	  }
 }
