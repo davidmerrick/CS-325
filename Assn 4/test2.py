ea8b6303a25bd5ef6ea8b9f356262d553b208269
jordannick <jordann@onid.orst.ede>
2013-11-18 22:03:54 -0800

    Maximize loop

---
+++ Assn 4/test2.py
@@ -0,0 +1,43 @@
+from pulp import *
+
+#Function definitions
+def find_slope(x1, y1, x2, y2):
+                return ((y2-y1)/(x2-x1))
+
+xs = [1, 2, 3, 5, 7, 8, 10]
+ys = [3, 5, 7, 11, 14, 15, 19]
+
+#Initialize LpProblem stuff
+
+
+#Find max and min slopes to put a bound on a
+min_slope = max_slope = find_slope(xs[0], ys[0], xs[1], ys[1])
+n = len(xs)
+for i in range(1, n):
+        for j in range(i+1, n):
+                slope = find_slope(xs[i], ys[i], xs[j], ys[j])
+                if(slope < min_slope):
+                        min_slope = slope
+                if(slope > max_slope):
+                        max_slope = slope
+
+
+for i in range(1, n):
+
+    prob = LpProblem("Best Fit Line Problem", LpMaximize)
+    a = LpVariable("a", 0)
+    b = LpVariable("b",0)
+    prob += a * xs[i] + b - ys[i]
+    status = prob.solve()
+    print value(prob.objective)
+    print "value of a: " + str(value(a))
+    print "value of b: " + str(value(b))
+
+
+
+
+
+print "Min possible slope: " + str(min_slope)
+print "Max possible slope: " + str(max_slope)
+
+
