d1e3229518c95f98e356ce0372a7cd3c1b609b82
jordannick <jordann@onid.orst.ede>
2013-11-18 20:38:49 -0800

    Different equations

--- Assn 4/test.py
+++ Assn 4/test.py
@@ -10,8 +10,10 @@ ys = [3, 5, 7, 11, 14, 15, 19]
 #Initialize LpProblem stuff
 prob = LpProblem("Best Fit Line Problem", LpMaximize)
 a = LpVariable("a",0)
-b = LpVariable("b",0)
+#b = LpVariable("b",0)
+b = 1;
 Z = LpVariable("Z",0)
+c = LpVariable("c",0)
 
 #Find max and min slopes to put a bound on a
 min_slope = max_slope = find_slope(xs[0], ys[0], xs[1], ys[1])
@@ -23,15 +25,20 @@ for i in range(1, n):
                         min_slope = slope
                 if(slope > max_slope):
                         max_slope = slope
-
-
-
+x = 0
+y = 0
 
 for i in range (1, n):
-	prob += (a * xs[i] + b - ys[i]) 
-	prob += Z - (a*xs[i]) >= -ys[i]
-	prob += Z + (a * xs[i]) >= ys[i]
+	x += xs[i]
+	y += ys[i]
 
+prob += (a * x + b - y) 
+
+for i in range (1, n):
+	#prob += (a * xs[i] + b - ys[i]) 
+
+	prob += a*xs[i]+b*ys[i]-c <= Z
+	prob += a*xs[i]+b*ys[i]-c >= -Z
 
 prob += a <= max_slope
 prob += a >= min_slope
@@ -45,3 +52,6 @@ print "Min possible slope: " + str(min_slope)
 print "Max possible slope: " + str(max_slope)
 print "value of a: " + str(value(a))
 print "value of b: " + str(value(b))
+print "value of c: " + str(value(c))
+
+
