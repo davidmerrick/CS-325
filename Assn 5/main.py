f4c84f1467dce1b1d6cccb16ed85bc9ad4d38632
David Merrick <david@david-merrick.com>
2013-12-02 21:18:38 -0800

    fixed point comparison

--- Assn 5/main.py
+++ Assn 5/main.py
@@ -16,22 +16,22 @@ from collections import namedtuple
 from operator import attrgetter
 
 Point = namedtuple('Point', 'index x y visited')
+pointlist = [] #create named tuple to store points and their coordinates
+
 
 def find_distance(x1, y1, x2, y2):
 	return ((x2-x1)**2 + (y2-y1)**2)**(.5)
 
 def get_nextclosest_x(pointlist, point):
+	print "point index = " + str(point.index)
 	#returns a point corresponding to the next closest unvisited X point
 	pointlist = sorted(pointlist, key=attrgetter('x'))
 	#Loop through the array until we find our current point
 	for i in pointlist:
 		#To do: not sure how to compare these namedtuple objects
-		if i.index == point.index
+		if i.index == point.index:
 			break
 	return i
-
-
-pointlist = [] #create named tuple to store points and their coordinates
 
 
 with open('example-input-1.txt', 'rb') as f:
@@ -39,5 +39,5 @@ with open('example-input-1.txt', 'rb') as f:
 	for row in reader:
 		pointlist.append(Point(row[0], row[1], row[2], 0));
  
-print "Original point is " + pointlist[5]
-print "point that it found is " + get_nextclosest_x(pointlist, pointlist[5])
+print "Original point is " + str(pointlist[5])
+print "point that it found is " + str(get_nextclosest_x(pointlist, pointlist[5]))
