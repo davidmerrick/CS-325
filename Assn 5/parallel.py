2fc7f4465cb71d6ac8276104e3a9e54a8cf7090e
David Merrick <david@david-merrick.com>
2013-12-05 15:29:58 -0800

    added random sampling of pointlist to spawn off threads with unique starting points

--- Assn 5/parallel.py
+++ Assn 5/parallel.py
@@ -21,7 +21,7 @@
 import csv
 from collections import namedtuple
 from operator import attrgetter
-from random import randint
+import random
 import threading 
 
 Point = namedtuple('Point', 'index x y')
@@ -36,9 +36,9 @@ def visited(point):
 	return true
 
 def get_nextclosest_x(pointlist, point):
-#returns a point corresponding to the next closest unvisited X point
+	#returns a point corresponding to the next closest unvisited X point
 	pointlist = sorted(pointlist, key=attrgetter('x'))
-#Loop through the array until we find our current point
+	#Loop through the array until we find our current point
 	j = 0
 	for i in pointlist:
 		if i.index == point.index:
@@ -74,27 +74,21 @@ def get_nextclosest_y(pointlist, point):
 			j+=1
 
 def nearest_neighbor(pointlist, starting_point):
-	print "pointlist = " 
-	print pointlist
-	#Initialize visited array
 	visited = []
-
-	#Visit the first node
 	current_point = starting_point
-
 	#Visit the rest of the nodes
 	while(len(pointlist) > 1):
-		previous_point = current_point
-		visited.append(previous_point)
-		closest_x = get_nextclosest_x(pointlist, previous_point)
-		closest_y = get_nextclosest_y(pointlist, previous_point)
-		distance_x = find_distance(previous_point, closest_x)
-		distance_y = find_distance(previous_point, closest_y)
-		if(distance_x < distance_y):
-			current_point = closest_x
-		else:
-			current_point = closest_y
-		pointlist.remove(previous_point)
+	        previous_point = current_point
+	        visited.append(previous_point)
+	        closest_x = get_nextclosest_x(pointlist, previous_point)
+	        closest_y = get_nextclosest_y(pointlist, previous_point)
+	        distance_x = find_distance(previous_point, closest_x)
+	        distance_y = find_distance(previous_point, closest_y)
+	        if(distance_x < distance_y):
+	                current_point = closest_x
+	        else:
+	                current_point = closest_y
+	        pointlist.remove(previous_point)
 
 	# Add the last point to pointlist
 	visited.append(pointlist.pop())
@@ -102,11 +96,13 @@ def nearest_neighbor(pointlist, starting_point):
 	#Calculate the tour distance
 	total_distance = 0
 	for i in range(0, len(visited)-1):
-		total_distance += find_distance(visited[i], visited[i+1])
+	        total_distance += find_distance(visited[i], visited[i+1])
 
 	#add on the distance from last visited point back to beginning
 	total_distance += find_distance(visited[0], visited[len(visited)-1])
-	print "Total distance: " + str(total_distance)
+
+	print "Total distance is " + str(total_distance)
+	print visited
 
 class FuncThread(threading.Thread):
     def __init__(self, target, *args):
@@ -124,12 +120,20 @@ with open('example-input-1-short.txt', 'rb') as f:
 	for row in reader:
 		pointlist.append(Point(row[0], row[1], row[2]))
 
+#Spawn off all the threads
+#Get random points from the list
+for i in range(len(pointlist)/2):
+	# get 40 random queries from the 50
+	starting_points = random.sample(pointlist, len(pointlist)/2);
 
-thread1 = FuncThread(nearest_neighbor, pointlist, pointlist[0])
-#thread1 = FuncThread(nearest_neighbor, pointlist, pointlist[0])
+#Spawn the threads
+threads = []
+j = 0
+for i in range(len(pointlist)/2):
+	thread = FuncThread(nearest_neighbor, pointlist, starting_points[j])
+	thread.start()
+	threads.append(thread)
+	j += 1
 
-thread1.start()
-#thread2.start()
-
-thread1.join()
-#thread2.join()
+for i in range(len(pointlist)/2):
+	threads[i].join()
